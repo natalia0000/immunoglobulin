5b6144a9c349d3de0c488466832671518e0dd3be
Natalia Zhukova <zhukova2000natasha@gmail.com>
2019-11-11 10:09:01 +0300

    Combining functions, restructuring

--- V-D.py
+++ V-D.py
@@ -5,115 +5,100 @@ from random import random
 from Bio.Seq import reverse_complement
 from Bio.Seq import complement
 
+p = 0.5  # вероятность для биномиального распределения остановки Артемиды
+p_compl = 0.8  # вероятность того, что хватит двух комплементарных нуклеотидов подряд, чтобы соединить цепочки
+p_number_seq = 0.5  # вероятность того, что нуклеотиды пристраиваются к первой цепочке
+A_weig = 15  # вес нуклеотида А
+C_weig = 35  # вес нуклеотида C
+T_weig = 15  # вес нуклеотида T
+G_weig = 35  # вес нуклеотида G
+exo_start_delete = 0.4  # вероятность того, что экзонуклеаза станет удалять нуклеотиды
+exo_stop_delete = 0.7  # вероятность того, что экзонуклеаза престанет удалять нуклеотиды
 
-def add_nucl_2(S_1, S_2, p_number_seq):
+
+def add_nucl(S_1, S_2, p_number_seq, count_nucl):
     version_for_1 = {}
     version_for_2 = {}
-    version_for_1[S_1[-2:]] = len(S_1) - 2
-    version_for_2[S_2[:2]] = len(S_2) - 2
-    while (version_for_2.get(complement(S_1[-2:])) is None) and (version_for_1.get(complement(S_2[:2])) is None):
-        if random() < p_number_seq:
-            S_1 = S_1 + choices(['A', 'C', 'T', 'G'], weights=[15, 35, 15, 35])[0]
-            version_for_1[S_1[-2:]] = len(S_1) - 2
+    for i in range(len(S_1) - count_nucl + 1):
+        if S_1[i:(i + count_nucl)] not in version_for_1:
+            version_for_1[S_1[i:(i + count_nucl)]] = [i]
         else:
-            S_2 = choices(['A', 'C', 'T', 'G'], weights=[15, 35, 15, 35])[0] + S_2
-            version_for_2[S_2[:2]] = len(S_2) - 2
+            version_for_1[S_1[i:(i + count_nucl)]].append(i)
+        if S_2[i:(i + count_nucl)] not in version_for_2:
+            version_for_2[S_2[i:(i + count_nucl)]] = [len(S_1) - i - count_nucl]
+        else:
+            version_for_2[S_2[i:(i + count_nucl)]].append(len(S_1) - i - count_nucl)
 
-    if version_for_2.get(complement(S_1[-2:])) is not None:
-        index = version_for_2.get(complement(S_1[-2:]))
+    while (complement(S_1[-count_nucl:]) not in version_for_2) and (
+            complement(S_2[:count_nucl]) not in version_for_1):
+        if random() < p_number_seq:
+            S_1 = S_1 + choices(['A', 'C', 'T', 'G'], weights=[A_weig, C_weig, T_weig, G_weig])[0]
+            if S_1[-count_nucl:] not in version_for_1:
+                version_for_1[S_1[-count_nucl:]] = [len(S_1) - count_nucl]
+            else:
+                version_for_1[S_1[-count_nucl:]].append(len(S_1) - count_nucl)
+        else:
+            S_2 = choices(['A', 'C', 'T', 'G'], weights=[A_weig, C_weig, T_weig, G_weig])[0] + S_2
+            if S_2[:count_nucl] not in version_for_2:
+                version_for_2[S_2[:count_nucl]] = [len(S_2) - count_nucl]
+            else:
+                version_for_2[S_2[:count_nucl]].append(len(S_2) - count_nucl)
+
+    if complement(S_1[-count_nucl:]) in version_for_2:
+        index = choice(version_for_2.get(complement(S_1[-count_nucl:])))
         S = S_1 + complement(S_2[(-index - 1):])
 
     else:
-        index = version_for_1.get(complement(S_2[:2]))
-        S = S_1[:index] + complement(S_2)
-
-    return S
-
-
-def add_nucl_3(S_1, S_2, p_number_seq):
-    version_for_1 = {}
-    version_for_2 = {}
-    version_for_1[S_1[-3:]] = len(S_1) - 3
-    version_for_2[S_2[:3]] = len(S_2) - 3
-    while (version_for_2.get(complement(S_1[-3:])) is None) and (version_for_1.get(complement(S_2[:3])) is None):
-        if random() < p_number_seq:
-            S_1 = S_1 + choice(['A', 'C', 'T', 'G'])
-            version_for_1[S_1[-3:]] = len(S_1) - 3
-        else:
-            S_2 = choice(['A', 'C', 'T', 'G']) + S_2
-            version_for_2[S_2[:3]] = len(S_2) - 3
-
-    if version_for_2.get(complement(S_1[-3:])) is not None:
-        index = version_for_2.get(complement(S_1[-3:]))
-        S = S_1 + complement(S_2[(-index - 1):])
-
-    else:
-        index = version_for_1.get(complement(S_2[:3]))
+        index = choice(version_for_1.get(complement(S_2[:count_nucl])))
         S = S_1[:index] + complement(S_2)
 
     return S
 
 
 def exo_work(S, exo_start_delete, exo_stop_delete):
+    n = len(S)
+    flag_arr = []
     status = 1
-    if random() < exo_start_delete:
+    if (exo_start_delete + exo_stop_delete) == 0:
+        print("Impossible probabilities")
+        return "Error"
+    if random() < (exo_start_delete)/(exo_start_delete + exo_stop_delete):
         status = 2
-    for i in range(len(S)):
+    for i in range(n):
         if status == 1:
+            flag_arr.append(1)
             if random() < exo_start_delete:
                 status = 2
         else:
-            S = S[:i] + S[(i + 1):]
-            i -= 1
+            flag_arr.append(0)
             if random() < exo_stop_delete:
                 status = 1
+    S_new = ""
+    for i in range(n):
+        if flag_arr[i]:
+            S_new = S_new + S[i]
+    return S_new
 
-    return S
 
+if __name__ == "__main__":
+    n = 3
+    DNA_pk = binom.rvs(n, p, 1)
 
-n = 3
-print("Введите вероятность для биномиального распределения остановки Артемиды:")
-p = float(input())
-# p = 0.5
+    S_1 = ''.join(choice(['A', 'C', 'T', 'G']) for _ in range(DNA_pk))
+    S_2 = ''.join(choice(['A', 'C', 'T', 'G']) for _ in range(DNA_pk))
 
-DNA_pk = binom.rvs(n, p, 1)
+    S_1 = S_1 + reverse_complement(S_1)
+    S_2 = S_2 + reverse_complement(S_2)
 
-S_1 = []
-S_2 = []
-for i in range(DNA_pk + 1):
-    S_1.append(choice(['A', 'C', 'T', 'G']))
-    S_2.append(choice(['A', 'C', 'T', 'G']))
-S_1 = ''.join(S_1)
-S_2 = ''.join(S_2)
+    print(DNA_pk, S_1, S_2)
 
-S_1 = S_1 + reverse_complement(S_1[1:(DNA_pk + 1)])
-S_2 = S_2 + reverse_complement(S_2[1:(DNA_pk + 1)])
+    if random() < p_compl:
+        count_nucl = 2
+    else:
+        count_nucl = 3
 
-print("Введите вероятность того, что хватит двух комплементарных нуклеотидов подряд, чтобы соединить цепочки:")
-p_compl = float(input())
-# p_compl = 0.8
-print("Введите вероятность того, что нуклеотиды пристраиваются к первой цепочке:")
-p_number_seq = float(input())
-# p_number_seq = 0.5
+    S = add_nucl(S_1, S_2, p_number_seq, count_nucl)
+    print(S)
 
-if random() < p_compl:
-    count_nukl = 2
-else:
-    count_nukl = 3
-
-if count_nukl == 2:
-    S = add_nucl_2(S_1, S_2, p_number_seq)
-
-else:
-    S = add_nucl_3(S_1, S_2, p_number_seq)
-print(S)
-
-print("Введите вероятность того, что экзонуклеаза станет удалять нуклеотиды:")
-exo_start_delete = float(input())
-print("Введите вероятность того, что экзонуклеаза престанет удалять нуклеотиды:")
-exo_stop_delete = float(input())
-# exo_start_delete = 0.4
-# exo_stop_delete = 0.55
-
-S = exo_work(S, exo_start_delete, exo_stop_delete)
-print(S)
+    S = exo_work(S, exo_start_delete, exo_stop_delete)
+    print(S)
